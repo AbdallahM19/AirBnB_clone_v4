c1db09055ae4855bcceeb6063f006e28d2f1e70d
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-05-05 04:15:30 -0700

    air_4

---
+++ models/user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import hashlib
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        if kwargs:
+            password = kwargs.pop('password', None)
+            if password:
+                hash_code = hashlib.md5()
+                hash_code.update(password.encode('utf-8'))
+                new_password = hash_code.hexdigest()
+                kwargs['password'] = new_password
+        super().__init__(*args, **kwargs)
